92499041f98f6f08e5d8520ca312f530ca919939
Stephen <mcaleer.stephen@gmail.com>
2016-11-19 18:56:44 -0700

    simple nb

---
+++ nb.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 19 18:43:17 2016
+
+@author: mcaleste
+"""
+
+from sklearn import datasets
+iris = datasets.load_iris()
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+print("Number of mislabeled points out of a total %d points : %d"
+      % (iris.data.shape[0],(iris.target != y_pred).sum()))
+
